50fa365892e17ed2021ae67fee6907a08ce6661b
hubiqing <hubiqing01@58ganji.com>
2017-08-02 16:43:48 +0800

    cwd

--- src/command/git.ts
+++ src/command/git.ts
@@ -43,12 +43,10 @@ export default {
             consoleColor.start(data.cmdStr)
             try {
                 //执行任务
-                await exec(data.cmdStr)
+                await exec(data.cmdStr, { cwd })
                 consoleColor.green('执行完毕', true)
             } catch (e) {
-                console.log('1111111')
                 consoleColor.error(e)
-                console.log('22222')
             }
         }
         consoleColor.green(`操作结束！`)
