8f05e6a663bef109e2011c1ae4c01439ecb812a2
hubiqing <hubiqing01@58ganji.com>
2017-09-09 13:24:13 +0800

    ignoreOK

--- src/command/ignore.ts
+++ src/command/ignore.ts
@@ -3,33 +3,34 @@ import { _, exec, getCurrentBranchName, cwd, consoleColor, io } from '../lib'
  * 生成ignore文件
  */
 export default {
-    /**
-     * 启动
-     */
-    async start(data) {
-        await this.copyIgnore(data.npm)
-    },
-    async copyIgnore(generateNpmIgnore) {
-        const fileNames = ['.gitignore']
-        if (generateNpmIgnore) {
-            fileNames.push('.npmignore')
-        }
-        for (let fileName of fileNames) {
-            const content = await io.read(fileName, { fromRoot: true })
-            consoleColor.green(`正在当前目录生成 ${fileName} 文件`)
-            await io.write(fileName, content, { fromCwd: true })
-        }
-        consoleColor.green(`生成成功 ${fileNames.join(',')}`, true)
-    },
-    command: [
-        'ignore',
-        '生成忽略文件',
-        {
-            npm: {
-                alias: ['n'],
-                boolean: true,
-                describe: '生成.npmignore'
-            },
-        }
-    ]
+  /**
+   * 启动
+   */
+  async start(data) {
+    await this.copyIgnore(data.npm)
+  },
+  async copyIgnore(generateNpmIgnore) {
+    const fileNames = ['template/template.gitignore']
+    if (generateNpmIgnore) {
+      fileNames.push('template/template.npmignore')
+    }
+    for (let fileName of fileNames) {
+      const content = await io.read(fileName, { fromRoot: true })
+      consoleColor.green(`正在当前目录生成 ${fileName} 文件`)
+      const newFilename = fileName.replace(/.*template/, '')
+      await io.write(newFilename, content, { fromCwd: true })
+      consoleColor.green(`生成成功 ${newFilename}`, true)
+    }
+  },
+  command: [
+    'ignore',
+    '生成忽略文件',
+    {
+      npm: {
+        alias: ['n'],
+        boolean: true,
+        describe: '生成.npmignore'
+      },
+    }
+  ]
 }
--- src/lib/io.ts
+++ src/lib/io.ts
@@ -5,50 +5,51 @@ import { stringify } from './other'
 import * as _ from 'lodash'
 import * as globby from 'globby'
 export const io = {
-    pathTool,
-    resolveOptions(path: string, options: any = { fromRoot: false, fromCwd: false }) {
-        path = pathTool.join.apply(null, [].concat(path))
-        path = pathTool.join.apply(null, (options.fromRoot ? [rootPath] : options.fromCwd ? [cwd] : []).concat(path))//考虑多路径处理
-        return path
-    },
-    globby(patterns: string | Array<string>, options: any = {}) { return globby(patterns, { dot: true, silent: true, strict: false, ...options }) },
-    read(path: string, options: any = { fromRoot: false, fromCwd: false }) {
-        path = this.resolveOptions(path, options)
-        return fs.readFileAsync(path, 'utf8')
-    },
-    write(path: string, content, options: any = { fromRoot: false, fromCwd: false }) {
-        path = this.resolveOptions(path, options)
-        //对对象进行 美化格式处理
-        content = _.isObject(content) ? stringify(content) : content
-        return fs.outputFileAsync(path, content)
-    }, delete(path) {
-        path = pathTool.join.apply(null, [].concat(path))
-        return fs.removeAsync(path)
-    },
-    exists(path, options: any = { fromRoot: false, fromCwd: false }) {
-        path = this.resolveOptions(path, options)
-        return fs.existsAsync(path)
-    },
-    /**
-     * 将文件或者路径 复制到另一个目录
-     */
-    copy(from: string, to: string) {
-        return fs.copyAsync(from, to)
-    },
-    /**
-     * 统一将路径分割符换成 /
-     */
-    replaceSep(path: string) {
-        return path.replace(/\\/g, '/')
-    },
-    /**
-     * 判断第一个参数是否是第二个参数的父路径   parent:  /t/c  child /t/c/d
-     */
-    isSubDir(parent: string | Array<string>, child: string | Array<string>) {
-        let parentPath: string = pathTool.resolve.apply(null, [].concat(parent))
-        parentPath = this.replaceSep(parentPath)
-        let childPath: string = pathTool.resolve.apply(null, [].concat(child))
-        childPath = this.replaceSep(childPath)
-        return childPath.startsWith(parentPath) && childPath.lastIndexOf('/') > parentPath.lastIndexOf('/')
-    }
+  pathTool,
+  resolveOptions(path: string, options: any = { fromRoot: false, fromCwd: false }) {
+    path = pathTool.join.apply(null, [].concat(path))
+    path = pathTool.join.apply(null, (options.fromRoot ? [rootPath] : options.fromCwd ? [cwd] : []).concat(path))//考虑多路径处理
+    return path
+  },
+  globby(patterns: string | Array<string>, options: any = {}) { return globby(patterns, { dot: true, silent: true, strict: false, ...options }) },
+  read(path: string, options: any = { fromRoot: false, fromCwd: false }) {
+    path = this.resolveOptions(path, options)
+    return fs.readFileAsync(path, 'utf8')
+  },
+  write(path: string, content, options: any = { fromRoot: false, fromCwd: false }) {
+    path = this.resolveOptions(path, options)
+    //对对象进行 美化格式处理
+    content = _.isObject(content) ? stringify(content) : content
+    return fs.outputFileAsync(path, content)
+  }
+  , delete(path) {
+    path = pathTool.join.apply(null, [].concat(path))
+    return fs.removeAsync(path)
+  },
+  exists(path, options: any = { fromRoot: false, fromCwd: false }) {
+    path = this.resolveOptions(path, options)
+    return fs.existsAsync(path)
+  },
+  /**
+   * 将文件或者路径 复制到另一个目录
+   */
+  copy(from: string, to: string) {
+    return fs.copyAsync(from, to)
+  },
+  /**
+   * 统一将路径分割符换成 /
+   */
+  replaceSep(path: string) {
+    return path.replace(/\\/g, '/')
+  },
+  /**
+   * 判断第一个参数是否是第二个参数的父路径   parent:  /t/c  child /t/c/d
+   */
+  isSubDir(parent: string | Array<string>, child: string | Array<string>) {
+    let parentPath: string = pathTool.resolve.apply(null, [].concat(parent))
+    parentPath = this.replaceSep(parentPath)
+    let childPath: string = pathTool.resolve.apply(null, [].concat(child))
+    childPath = this.replaceSep(childPath)
+    return childPath.startsWith(parentPath) && childPath.lastIndexOf('/') > parentPath.lastIndexOf('/')
+  }
 }
