d395fa8177ce861e819ef7c39f64aaa288106eac
hubiqing <hubiqing01@58ganji.com>
2017-12-06 14:00:13 +0800

    sync

--- src/command/npm.ts
+++ src/command/npm.ts
@@ -4,44 +4,54 @@ import { _, exec, getCurrentBranchName, cwd, consoleColor, io } from '../lib'
  * 配置或者查看npm registry
  */
 export default {
-    /**
-     * 启动
-     */
-    async start(data) {
-        const setRegistryStr = `npm config set registry https://registry.npm.taobao.org`
-        const setNpmRegistryStr = `npm config set registry https://registry.npmjs.org`
-        await this.showCurrentRegistry()
-        if (data.taobao) {
-            consoleColor.start(`${setRegistryStr}`)
-            await exec(setRegistryStr)
-        } else if (data.npm) {
-            consoleColor.start(`${setNpmRegistryStr}`)
-            await exec(setNpmRegistryStr)
-        }
-        if (data.taobao || data.npm) {
-            await this.showCurrentRegistry()
-        }
-        consoleColor.green(`操作结束！`)
+  /**
+   * 启动
+   */
+  async start(data) {
+    const registryStr = `npm config set registry https://registry.npm.taobao.org`
+    const npmRegistryStr = `npm config set registry https://registry.npmjs.org`
+    const zhaopinRegistryStr = `npm config set registry http://npm.zhaopin.com`
+    await this.showCurrentRegistry()
+    if (data.taobao) {
+      consoleColor.start(`${registryStr}`)
+      await exec(registryStr)
+    } else if (data.npm) {
+      consoleColor.start(`${npmRegistryStr}`)
+      await exec(npmRegistryStr)
+    }
+    else if (data.zhaopin) {
+      consoleColor.start(`${zhaopinRegistryStr}`)
+      await exec(zhaopinRegistryStr)
+    }
+    if (data.taobao || data.npm) {
+      await this.showCurrentRegistry()
+    }
+    consoleColor.green(`操作结束！`)
+  },
+  async showCurrentRegistry() {
+    consoleColor.green('当前 npm registry 地址:')
+    await exec('npm config get registry')
+  },
+  command: [
+    'npm',
+    '配置或者查看npm registry',
+    {
+      taobao: {
+        alias: ['t'],
+        boolean: true,
+        describe: '将 npm 默认 registry 设置为https://registry.npm.taobao.org'
+      },
+      npm: {
+        alias: ['n'],
+        boolean: true,
+        describe: '将 npm 默认 registry 设置为https://registry.npmjs.org'
+      },
+      zhaopin: {
+        alias: ['z'],
+        boolean: true,
+        describe: '将 npm 默认 registry 设置为http://npm.zhaopin.com'
+      }
     },
-    async showCurrentRegistry() {
-        consoleColor.green('当前npm registry地址:')
-        await exec('npm config get registry')
-    },
-    command: [
-        'npm',
-        '配置或者查看npm registry',
-        {
-            taobao: {
-                alias: ['t'],
-                boolean: true,
-                describe: '将npm 默认registry 设置为https://registry.npm.taobao.org'
-            },
-            'npm': {
-                alias: ['n'],
-                boolean: true,
-                describe: '将npm 默认registry 设置为https://registry.npmjs.org'
-            }
-        },
 
-    ]
+  ]
 }
--- src/command/yarn.ts
+++ src/command/yarn.ts
@@ -4,44 +4,54 @@ import { _, exec, getCurrentBranchName, cwd, consoleColor, io } from '../lib'
  * 配置或者查看yarn registry
  */
 export default {
-    /**
-     * 启动
-     */
-    async start(data) {
-        const setRegistryStr = `yarn config set registry https://registry.npm.taobao.org`
-        const setNpmRegistryStr = `yarn config set registry https://registry.npmjs.org`
-        await this.showCurrentRegistry()
-        if (data.taobao) {
-            consoleColor.start(`${setRegistryStr}`)
-            await exec(setRegistryStr)
-        } else if (data.npm) {
-            consoleColor.start(`${setNpmRegistryStr}`)
-            await exec(setNpmRegistryStr)
-        }
-        if (data.taobao || data.npm) {
-            await this.showCurrentRegistry()
-        }
-        consoleColor.green(`操作结束！`)
+  /**
+   * 启动
+   */
+  async start(data) {
+    const registryStr = `yarn config set registry https://registry.npm.taobao.org`
+    const npmRegistryStr = `yarn config set registry https://registry.npmjs.org`
+    const zhaopinRegistryStr = `yarn config set registry http://npm.zhaopin.com`
+    await this.showCurrentRegistry()
+    if (data.taobao) {
+      consoleColor.start(`${registryStr}`)
+      await exec(registryStr)
+    } else if (data.npm) {
+      consoleColor.start(`${npmRegistryStr}`)
+      await exec(npmRegistryStr)
+    }
+    else if (data.zhaopin) {
+      consoleColor.start(`${zhaopinRegistryStr}`)
+      await exec(zhaopinRegistryStr)
+    }
+    if (data.taobao || data.npm) {
+      await this.showCurrentRegistry()
+    }
+    consoleColor.green(`操作结束！`)
+  },
+  async showCurrentRegistry() {
+    consoleColor.green('当前 yarn registry地址:')
+    await exec('yarn config get registry')
+  },
+  command: [
+    'yarn',
+    '配置或者查看 yarn registry',
+    {
+      taobao: {
+        alias: ['t'],
+        boolean: true,
+        describe: '将 yarn 默认 registry 设置为https://registry.npm.taobao.org'
+      },
+      npm: {
+        alias: ['n'],
+        boolean: true,
+        describe: '将 yarn 默认 registry 设置为https://registry.npmjs.org'
+      },
+      zhaopin: {
+        alias: ['z'],
+        boolean: true,
+        describe: '将 yarn 默认 registry 设置为http://npm.zhaopin.com'
+      }
     },
-    async showCurrentRegistry() {
-        consoleColor.green('当前yarn registry地址:')
-        await exec('yarn config get registry')
-    },
-    command: [
-        'yarn',
-        '配置或者查看yarn registry',
-        {
-            taobao: {
-                alias: ['t'],
-                boolean: true,
-                describe: '将yarn 默认registry 设置为https://registry.npm.taobao.org'
-            },
-            'npm': {
-                alias: ['n'],
-                boolean: true,
-                describe: '将npm 默认registry 设置为https://registry.npmjs.org'
-            }
-        },
 
-    ]
+  ]
 }
