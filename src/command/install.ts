acb77e723334e8d3c2b5d5075335ec44a62eb4f3
biqing.hu <biqing.hu@zhaopin.com.cn>
2018-12-29 11:09:06 +0800

    install

--- src/command/install.ts
+++ src/command/install.ts
@@ -1,4 +1,4 @@
-import { _, _Promise, exec, getCurrentBranchName, cwd, consoleColor, io, packageHelper } from '../lib'
+import { _, exec, getCurrentBranchName, cwd, consoleColor, io } from '../lib'
 import show from './show'
 import * as Listr from 'listr'
 import { Observable } from 'rxjs/Observable'
@@ -19,50 +19,20 @@ export default {
         let tool = data.npm ? 'npm' : 'yarn'
         for (let pathModel of pathModels) {
             tasks.push({
-                title: `执行 ${tool} install @ ${pathModel.path}`,
-                task: async () => {
-                    debugger
-                    const packageJson = await packageHelper.get(io.pathTool.resolve(pathModel.path))
-                    const { dependencies, devDependencies } = packageJson
-                    const existsPackages = { ...dependencies, ...devDependencies }
-                    const promiseTasks = []
-                    const fileLibPromiseTasks = []
-                    debugger
-                    Object.keys(existsPackages).forEach(key => {
-                        const value = existsPackages[key]
-                        let cmdStr
-                        let taskQueue = promiseTasks
-                        if (value.trim().indexOf('file:') === 0) {
-                            taskQueue = fileLibPromiseTasks
-                        }
-                        if (data.npm) {
-                            cmdStr = `${tool} install ${key}@${value} --no-save --no-shrinkwrap --no-package-lock`
-                        } else {
-                            cmdStr = `${tool} install ${key}@${value} --no-save --no-lockfile`
-                        }
-                        taskQueue.push({ cmdStr, options: { cwd: pathModel.path, preventDefault: true } })
-
-                    })
-
-                    const result = _Promise.map(
-                        [...promiseTasks, ...fileLibPromiseTasks],
-                        (n) => {
-                            return exec(n.cmdStr, n.options).catch(() => { })
-                        },
-                        { concurrency: 1 }
-                    )
-                    return result
+                title: `执行 ${tool} install @ ${pathModel.path} --no-save`,
+                task: () => {
+                    return exec(`${tool} install --no-save `, { cwd: pathModel.path, preventDefault: true }).catch(() => { })
                 }
             })
         }
-        const taskManager = new Listr(tasks, { concurrent: 1 })
+        const taskManager = new Listr(tasks, { concurrent: true })
         await taskManager.run()
         consoleColor.timeEnd('总耗时')
         consoleColor.green(`${tool} install 完毕\n\n`, true)
     },
     command: [
         'install',
-        '默认执行 yarn install',
+        '执行yarn install',
         {
             npm: {
                 alias: 'n',
