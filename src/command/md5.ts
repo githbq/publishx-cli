fcc139e0d0f889ae22db61be74ae7ae97f46e9d2
biqing.hu <biqing.hu@zhaopin.com.cn>
2019-04-09 13:16:20 +0800

    ok

--- src/command/md5.ts
+++ src/command/md5.ts
@@ -10,7 +10,7 @@ export default {
   /**
    * 启动
    */
-  async start({ target }) { 
+  async start({ target }) {
     if (target.indexOf('http') !== -1) {
       const hash = crypto.createHash('md5')
       const { data } = await axios.get(target)
@@ -21,22 +21,22 @@ export default {
     else {
       let files = []
       if (await io.fs.isDirectoryAsync(io.pathTool.join(cwd, target))) {
-        files = await io.globby('*.*', { dot: false, nodir: true })
+        files = await io.globby(['*.*', '!*.map'], { cwd: io.pathTool.join(cwd, target), absolute: true, dot: false, nodir: true })
       } else {
         files = [target]
-      } 
+      }
       for (let file of files) {
         if (await io.exists(file)) {
           const hash = crypto.createHash('md5')
           const content = await io.read(file)
           hash.update(content)
           const contentHash = hash.digest('hex')
-          consoleColor.green(`路径:${file} => hash:[${contentHash}]    || 文件内容长度:${content.length}`)
+          consoleColor.green(`路径:${io.pathTool.relative(cwd, file)} => hash:[${contentHash}]    || 文件内容长度:${content.length}`)
         }
         else {
           consoleColor.red(`路径文件:${file}不存在`)
         }
-      } 
+      }
     }
   },
   command: ['md5 <target>', '查看远程或者本地文件的md5值',
