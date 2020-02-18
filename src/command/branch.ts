5039849ef18ba781907dc080927b02c886aa9f89
hubiqing <hbqhbq123@qq.com>
2020-02-18 16:31:49 +0800

    branch

---
+++ src/command/branch.ts
@@ -0,0 +1,42 @@
+import { _, packageHelper, exit, exec, getCurrentBranchName, consoleColor, cwd, io } from '../lib'
+
+/**
+ * 推送代码到远程服务器
+ */
+export default {
+  /**
+   * 启动
+   */
+  async start(data) {
+    const arr = [
+      {
+        tip: '当前分支',
+        cmdStr: 'git branch'
+      },
+      {
+        tip: '本地分支列表',
+        cmdStr: 'git branch -a'
+      },
+      {
+        tip: '远程分支列表',
+        cmdStr: 'git branch -r'
+      }
+    ]
+    consoleColor.time('耗时')
+    for (let item of arr) {
+      try {
+        consoleColor.start(item.tip)
+        await exec(item.cmdStr)
+      }
+      catch (e) {
+        consoleColor.red(`发生异常:${e.message}`, false)
+      }
+    }  
+    consoleColor.timeEnd('耗时')
+  },
+  command: [
+    'branch', '显示分支信息',
+    {
+    }
+  ]
+}
