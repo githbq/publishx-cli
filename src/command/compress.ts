ad4499e0ff0da618b5de2c53b415694c87a2afc8
hubiqing <hbqhbq123@qq.com>
2020-08-07 16:21:51 +0800

    compress

--- src/command/compress.ts
+++ src/command/compress.ts
@@ -1,5 +1,5 @@
 import { _, packageHelper, exit, exec, getCurrentBranchName, consoleColor, cwd, io } from '../lib'
-import * as  zipPaths from 'zip-paths'
+import GlobbyCompress from 'globby-compress'
 /**
  * 忽略node_modules与.git对当前工程压缩成一个zip包放置在dist文件夹下
  */
@@ -9,38 +9,25 @@ export default {
      */
     async start() {
         consoleColor.time('耗时')
-        await io.ensureDir('./dist')
-        consoleColor.green('确认./dist目录', true)
         const zipPath = `./dist/${packageHelper.getProjectName()}.zip`
-        consoleColor.green('索引文件中...')
-        const zip = new zipPaths(zipPath,
-            { archiveType: 'zip' })
-        await new Promise(resolve => {
-            zip.add('**/*', {
-                cwd: cwd,
-                dot: true,
-                ignore: [
-                    '**/node_modules/**/*',
-                    '**/dist/**/*',
-                    '**/.git/**/*',
-                    '**/temp.*',
-                    '**/.DS_Store/**/*',
-                    '**/.DS_Store'
-                ]
-            },
-                function (err) {
-                    if (err) {
-                        consoleColor.error(err)
-                        resolve()
-                    }
-                    consoleColor.green('压缩文件夹中...')
-                    zip.compress((err, bytes) => {
-                        consoleColor.green('压缩完成，压缩包路径:' + zipPath, true)
-                        consoleColor.green('共' + (Math.floor(bytes / 1024) || 0) + 'KB', true)
-                        resolve()
-                    })
-                })
-        })
+        const globbyCompress = new GlobbyCompress(zipPath,
+            [
+                '**/*',
+                '!**/node_modules/**/*',
+                '!**/dist/**/*',
+                '!**/.git/**/*',
+                '!**/temp.*',
+                '!**/.DS_Store/**/*',
+                '!**/.DS_Store'
+            ], { cwd })
+        try {
+            consoleColor.green('压缩文件夹中...')
+            const bytes: any = await globbyCompress.compress()
+            consoleColor.green('压缩完成，文件路径:' + zipPath, true)
+            consoleColor.green('共' + (Math.floor(bytes / 1024) || 0) + 'KB')
+        } catch (e) {
+            console.error(e)
+        }
         consoleColor.timeEnd('耗时')
     },
     command: [
