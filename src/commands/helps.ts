f2a2aefcd9ff08a1948746ef090c3fb6de7f0733
unknown <hbq>
2020-08-09 18:45:38 +0800

    helps

--- src/commands/helps.ts
+++ src/commands/helps.ts
@@ -2,6 +2,16 @@
 
 import { _, packageHelper, exit, exec, getCurrentBranchName, consoleColor, cwd, io } from '../lib'
 
+class Help {
+  public description: string
+  public solution: string
+  constructor(description, solution) {
+    this.description = description
+    this.solution = solution
+
+  }
+
+}
 /**
  * 帮助信息
  */
@@ -10,9 +20,17 @@ export default {
    * 启动
    */
   async start(data) {
-    consoleColor.green(
-      `macos下无法安装brew解决方法:
-         $>:  /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"`)
+    const helps = [
+      new Help('macos下无法安装brew', '$>:  /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"'),
+      new Help('macos nginx 安装目录在哪？', '1. macos nginx 安装目录 /usr/local/etc/nginx ; 2. nginx -V')
+    ]
+
+    helps.forEach(n => {
+      consoleColor.green(
+        `问题描述：${n.description}
+         解决方案：${n.solution}
+        `)
+    })
   },
   command: [
     '帮助信息 ',
