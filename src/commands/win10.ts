668d27f852908f83783bff8265758365f6f2c526
hubiqing <hubiqing@jd.com>
2021-02-19 10:15:42 +0800

    ok

--- src/commands/win10.ts
+++ src/commands/win10.ts
@@ -11,6 +11,12 @@ export default {
     async start(data) {
         if (checkOS.windows) {
             // px : 无法加载文件 C:\Users\vhouyuewei\AppData\Roaming\npm\px.ps1，
+            // 解决办法是：
+            // 以管理员身份运行PowerShell
+            // 执行：get-ExecutionPolicy，回复Restricted，表示状态是禁止的
+            // 执行：set-ExecutionPolicy RemoteSigned
+            // 选择Y
+            // 注意：一定要以管理员的身份运行PowerShell，不是cmd窗口！
             const cmdStr = `set-ExecutionPolicy RemoteSigned`
             consoleColor.start(cmdStr)
             try {
