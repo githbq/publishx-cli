78dd69f06811dcebce36513cb8882154676d1012
hubiqing <hubiqing@jd.com>
2021-03-04 15:15:01 +0800

    checkout

--- src/commands/checkout.ts
+++ src/commands/checkout.ts
@@ -1,4 +1,5 @@
 import { _, packageHelper, exit, exec, getCurrentBranchName, consoleColor, cwd, io } from '../lib'
+import show from './show'
 
 /**
  * git checkout
@@ -8,23 +9,33 @@ export default {
    * 启动
    */
   async start({ name, branch }) {
-    const cmdStr = `git remote update && git checkout ${branch ? '-b' : ''} ${name}`
-    consoleColor.start(cmdStr) 
-    try {
-      await exec(cmdStr)
-    }
-    catch (e) {
-      // consoleColor.red(`发生异常:${e.message}`, false)
-    } 
+    const pathModels = await show.findProjectPaths()
+
+    const promises = pathModels.map(async (n) => {
+      if (!n.isGit) {
+        consoleColor.red(`工程:${n.path} 非git工程，跳过`)
+        return Promise.resolve()
+      }
+      const cmdStr = `git remote update && git checkout ${branch ? '-b' : ''} ${name}`
+      consoleColor.start(cmdStr)
+      try {
+        await exec(cmdStr, { cwd: n.path })
+      }
+      catch (e) {
+        consoleColor.red(`发生异常:${e.message}`, false)
+      }
+    })
+    await Promise.all(promises)
+
   }, command:
-  [
-    '<name>', 'git remote update && git checkout <branchName>',
-    {
-      branch: {
-        alias: ['b'],
-        boolean: true,
-        describe: '创建 并切换到新分支'
-      },
-    }
-  ]
+    [
+      '<name>', 'git remote update && git checkout <branchName>',
+      {
+        branch: {
+          alias: ['b'],
+          boolean: true,
+          describe: '创建 并切换到新分支'
+        },
+      }
+    ]
 }
