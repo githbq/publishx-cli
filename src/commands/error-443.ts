3fb16efcaa8ed4e5e12b2b1f00df53005a44376f
biqing.hu <biqing.hu@okg.com>
2022-02-15 14:27:33 +0800

    error-443

---
+++ src/commands/error-443.ts
@@ -0,0 +1,36 @@
+// npm config set sass-binary-path D:/node-binaries/node-sass-win32-x64-57_binding.node
+
+// yarn config set sass-binary-path D:/node-binaries/node-sass-win32-x64-57_binding.node
+//https://npm.taobao.org/mirrors/node-sass/v4.5.3/win32-x64-57_binding.node
+
+
+import { _, exec, getCurrentBranchName, cwd, consoleColor, io, checkOS } from '../lib'
+/**
+ * 配置sass
+ */
+export default {
+    /**
+     * 启动
+     */
+    async start(data) {
+        const cmdStrs = [
+            'git config --global --add remote.origin.proxy ""',
+            'git config --global --unset-all remote.origin.proxy',
+            'git config --global http.sslVerify false'
+        ] 
+        for (let cmdStr of cmdStrs) {
+            try {
+                consoleColor.start(cmdStr)
+                await exec(cmdStr)
+            } catch (e) {
+                consoleColor.error(e)
+            }
+        } 
+    },
+    command: [ 
+        `fatal: unable to access 'https://github.com/xxxxx.git/': Failed to connect to github.com port 443: Operation timed out`,
+        {
+
+        }
+    ]
+}
