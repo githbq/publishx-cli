533c480ab533a56a81f8e0d2ce8ea0ccb89a687f
biqing.hu <biqing.hu@okg.com>
2022-08-21 10:42:52 +0800

    clear-debug

--- src/commands/clear-debugger.ts
+++ src/commands/clear-debugger.ts
@@ -11,25 +11,25 @@ export default {
      */
     async start(data) {
         const result = await io.globby(
-            ['{packages,src,scripts}/**/*.[j]s', '!**/node_modules/**'],
+            ['{packages,src,scripts}/**/*.[t,j]s', '!**/node_modules/**'],
             {
                 nodir: true,
             },
-        );
+        )
         console.log(result)
         const promises = result.map(async filePath => {
-            const content = await io.read(filePath);
+            const content = await io.read(filePath)
 
-            const lines = content.split(EOL);
+            const lines = content.split(EOL)
 
             const filteredLines = lines.filter(
                 line => !/^\s*debugger;?\s*$/.test(line),
-            );
-            const newContent = filteredLines.join(EOL);
-            await io.write(filePath, newContent);
-        });
-        await Promise.all(promises);
-        consoleColor.green('clear ts&js file debugger completed', true);
+            )
+            const newContent = filteredLines.join(EOL)
+            await io.write(filePath, newContent)
+        })
+        await Promise.all(promises)
+        consoleColor.green('clear ts&js file debugger completed', true)
     },
     command: [
         '清理代码中的 debuger 行',
