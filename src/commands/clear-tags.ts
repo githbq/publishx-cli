e968224cb2a25ef461cd651a8eb2e0340bb1e97a
biqing.hu <biqing.hu@okg.com>
2024-05-10 17:10:51 +0800

    tags

--- src/commands/clear-tags.ts
+++ src/commands/clear-tags.ts
@@ -1,4 +1,5 @@
 import { _, packageHelper, exit, confirm, exec, getCurrentBranchName, consoleColor, cwd, io } from '../lib'
+import * as Listr from 'listr'
 
 /**
  * 删除 node_modules
@@ -17,18 +18,27 @@ export default {
         const yes = await confirm('是否要删除掉所有tags?')
         if (yes) {
           let count = 0
+          let tasks = []
           for (let tag of tags) {
-            try {
-              count++
-              const cmdDeleteTagStr = `git tag --delete ${tag} && git push origin :${tag}`
-              consoleColor.start(cmdDeleteTagStr) 
-              await exec(cmdDeleteTagStr) 
-            } catch (e) {
-              consoleColor.error(e)
-            } finally {
-              consoleColor.green(`剩余${tags.length - count}项`)
-            }
+            tasks.push({
+              title: `[count]: delete tag ${tag}`,
+              task: async () => {
+                try {
+                  count++
+                  const cmdDeleteTagStr = `git tag --delete ${tag} && git push origin :${tag}`
+                  consoleColor.start(cmdDeleteTagStr)
+                  await exec(cmdDeleteTagStr)
+                } catch (e) {
+                  consoleColor.error(e)
+                } finally {
+                  consoleColor.green(`剩余${tags.length - count}项`)
+                }
+              }
+            })
+
           }
+          const taskManager = new Listr(tasks, { concurrent: data.concurrent || 20 })
+          await taskManager.run()
 
         }
       } catch (error) {
@@ -54,6 +64,11 @@ export default {
   command: [
     '<tagNames>', '多个tag以,分割  git tag --delete [tagName] && git push origin :refs/tags/[tagName]',
     {
+      concurrent: {
+        alias: ['c'],
+        number: true,
+        describe: '并发数'
+      },
     }
   ]
 }
